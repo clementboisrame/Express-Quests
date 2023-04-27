83dc14305994361658bd580f095c62205ce99a4b
Clément Boisrame <clement.boisrame@laposte.net>
2023-04-27 21:42:10 +0200

    challenge express02

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express");
-
 const app = express();
+const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
 
-const port = 5000;
-
-const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
-};
+const welcome = (req, res) => { res.send("Welcome to my favourite movie list"); };
 
 app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
+// Routes pour les films
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+// Routes pour les utilisateurs
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
+const port = process.env.APP_PORT ?? 5000;
 
 app.listen(port, (err) => {
   if (err) {
